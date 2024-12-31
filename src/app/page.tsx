319229c715f173896196af1576003122bd07b184
MoPaMo <mopamo@users.noreply.github.com>
2024-12-31 10:23:00 +0100

    wrap xkcd-Passwords heading in a header element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>xkcd-Passwords</h1>
+      <header>
+        <h1>xkcd-Passwords</h1>
+      </header>
     </div>
   );
 }
