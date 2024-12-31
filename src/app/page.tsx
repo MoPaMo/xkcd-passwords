1ffa155afcd7b7032cb026923c30c8b9aa353c98
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-12-31 15:48:15 +0000

    feat: integrate InfoCard component to display security information on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import GeneratorCard from "@/components/GeneratorCard";
+import InfoCard from "@/components/InfoCard";
+import { Shield, Zap } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <GeneratorCard />
+      <InfoCard icon={Shield} title="Security" iconColor="text-green-500">
+        This method provides high entropy{" "}
+      </InfoCard>
     </div>
   );
 }
