3ecb12160e0d503dc097d25fd04ad11d0f8fba01
MoPaMo <mopamo@users.noreply.github.com>
2024-12-31 12:42:51 +0100

    add useToast hook and integrate GeneratorCard component in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "@/components/Header";
+import GeneratorCard from "@/components/GeneratorCard";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <GeneratorCard />
     </div>
   );
 }
